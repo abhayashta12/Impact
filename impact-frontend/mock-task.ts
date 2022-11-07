399b3da2286cba127393880188f9711b8580327c
Abhay Ashta <aashta2@my.centennialcollege.ca>
2022-11-07 00:15:29 -0500

    first commit

---
+++ impact-frontend/mock-task.ts
@@ -0,0 +1,14 @@
+import {Task} from "Task";
+
+export const TASKS : Task[] = [
+    {
+        text:'Random task for me to do today',
+        day: 'May 08th, Wednesday',
+        reminder: true
+    },
+    {
+        text:'Random Task i am not going to do',
+        day: 'May 25th at 18:30',
+        reminder: true
+    }
+]
